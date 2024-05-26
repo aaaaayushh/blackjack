186a30ad3ee84d4fb6bf088a6047222f6447408e
AayushShah <aayushshah1142@gmail.com>
2024-05-26 14:22:22 +0530

    basic scoring logic, simple dealer interaction

--- main.go
+++ main.go
@@ -20,7 +20,27 @@ func (h Hand) String() string {
 func (h Hand) DealerString() string {
 	return h[0].String() + ", **HIDDEN**"
 }
-
+func (h Hand) MinScore() int {
+	score := 0
+	for _, c := range h {
+		score += min(int(c.Rank), 10)
+	}
+	return score
+}
+func (h Hand) Score() int {
+	minScore := h.MinScore()
+	// if minScore is > 11, then we can't use the ace as 11
+	if minScore > 11 {
+		return minScore
+	}
+	for _, c := range h {
+		if c.Rank == deck.Ace {
+			// ace is worth 1, we are changing it to 11
+			return minScore + 10
+		}
+	}
+	return minScore
+}
 func main() {
 	// 3 decks of cards, shuffled
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
@@ -46,9 +66,29 @@ func main() {
 			player = append(player, card)
 		}
 	}
+	// if dealer's score <= 16, then dealer has to hit
+	// if dealer has a soft 17, then dealer has to hit
+
+	for dealer.Score() <= 16 || (dealer.Score() == 17 && dealer.MinScore() != 17) {
+		card, cards = draw(cards)
+		dealer = append(dealer, card)
+	}
+	pScore, dScore := player.Score(), dealer.Score()
 	fmt.Println("==FINAL HANDS==")
-	fmt.Println("Player:", player)
-	fmt.Println("Dealer:", dealer)
+	fmt.Println("Player:", player, "\nScore:", pScore)
+	fmt.Println("Dealer:", dealer, "\nScore:", dScore)
+	switch {
+	case pScore > 21:
+		fmt.Println("BUST!")
+	case dScore > 21:
+		fmt.Println("DEALER BUSTS!")
+	case pScore > dScore:
+		fmt.Println("PLAYER WINS!")
+	case dScore > pScore:
+		fmt.Println("DEALER WINS!")
+	case dScore == pScore:
+		fmt.Println("DRAW!")
+	}
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
